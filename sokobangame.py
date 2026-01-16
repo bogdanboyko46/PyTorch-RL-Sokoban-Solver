a86e1301ac63e26a564545b3e6581c495a4485dc
oisinmaher <oisinmaher527@gmail.com>
2026-01-16 00:02:47 +0000

    added all move directions, for example the player can now push the block down and then wrap around to the blocks left and push it right

--- sokobangame.py
+++ sokobangame.py
@@ -12,50 +12,62 @@ player = []
 def main():
     global grid
     global player
-    grid = [['e', 'h', 'e'], ['e', 'b', 'e'], ['e', 'e', 'e']]
-    player = [2, 1]
+    grid = [['e', 'e', 'e'], ['e', 'b', 'e'], ['e', 'e', 'e']]
+    player = [0, 1]
     print("before")
     print(f"player location {player}")
     for g in grid:
         print(g)
-    move_up()
+    move_down()
+    print(f"player location {player}")
+    move_left()
+    print(f"player location {player}")
+    move_down()
+    print(f"player location {player}")
+    move_right()
     print("after")
     print(f"player location {player}")
     for g in grid:
         print(g)
 def move_up() -> bool:
+    return move_vertical(-1)
+def move_down() -> bool:
+    return move_vertical(1)
+def move_left() -> int:
+    return move_horizontal(-1)
+def move_right() -> int:
+    return move_horizontal(1)
+
+def move_vertical(dir: int) -> bool:
     # player cur pos
     global player
     y = player[0]
     x = player[1]
-
-    # the soon to be y position (-1 because moving up)
-    newy = y - 1
-
-    # The y grid 2 grids above current (needed if pushing a block up)
-    abovey = y-2
+    newy = y + dir
+    fary = y + (dir * 2)
+    gridlen = len(grid)
 
     # if out of bounds or above object is a wall return false (not possible to move)
-    if newy < 0 or grid[newy][x] == 'w':
+    if newy < 0 or newy >= gridlen or grid[newy][x] == 'w':
         return False
 
-    # if above object is empty space or a free hole (moves up)
+    # if above object is empty space or a free hole
     elif grid[newy][x] == 'e' or grid[newy][x] == 'h':
         player = [newy, x]
 
     # if above object is a block, or a hole with block in it
     elif grid[newy][x] == 'c' or grid[newy][x] == 'b':
         # if the next above object is oob or not vacant space
-        if abovey < 0 or grid[abovey][x] == 'w' or grid[abovey][x] == 'c' or grid[abovey][x] == 'b':
+        if fary < 0 or fary >= gridlen or grid[fary][x] == 'w' or grid[fary][x] == 'c' or grid[fary][x] == 'b':
             return False
 
         # updates grid coord above (where the block is being pushed)
         # empty -> block
-        if grid[abovey][x] == 'e':
-            grid[abovey][x] = 'b'
+        if grid[fary][x] == 'e':
+            grid[fary][x] = 'b'
         # hole -> complete (because block went into hole so this grid is 'complete' (can still change))
         else:
-            grid[abovey][x] = 'c'
+            grid[fary][x] = 'c'
 
         # updates new coord (where block was and players moving to)
         # block -> empty (because block has been moved)
@@ -67,6 +79,49 @@ def move_up() -> bool:
 
         # update player location
         player = [newy, x]
+    return True
+
+def move_horizontal(dir: int) -> bool:
+    global player
+    y = player[0]
+    x = player[1]
+    newx = x + dir
+    farx = x + (dir * 2)
+    gridlen = len(grid[0])
+
+
+    # if out of bounds or above object is a wall return false (not possible to move)
+    if newx < 0 or newx >= gridlen or grid[y][newx] == 'w':
+        return False
+
+    # if above object is empty space or a free hole
+    elif grid[y][newx] == 'e' or grid[y][newx] == 'h':
+        player = [y, newx]
+
+    # if above object is a block, or a hole with block in it
+    elif grid[y][newx] == 'c' or grid[y][newx] == 'b':
+        # if the next above object is oob or not vacant space
+        if farx < 0 or farx >= gridlen or grid[y][farx] == 'w' or grid[y][farx] == 'c' or grid[y][farx] == 'b':
+            return False
+
+        # updates grid coord above (where the block is being pushed)
+        # empty -> block
+        if grid[y][farx] == 'e':
+            grid[y][farx] = 'b'
+        # hole -> complete (because block went into hole so this grid is 'complete' (can still change))
+        else:
+            grid[y][farx] = 'c'
+
+        # updates new coord (where block was and players moving to)
+        # block -> empty (because block has been moved)
+        if grid[y][newx] == 'b':
+            grid[y][newx] = 'e'
+        # complete -> hole (block was pushed from hole)
+        else:
+            grid[y][newx] = 'h'
+
+        # update player location
+        player = [y, newx]
     return True
 
 main()
